9ecdac3627b15b9c411285126e5d9e4b39174d57
juapew2021 <62611754+juapew2021@users.noreply.github.com>
2024-11-13 18:10:55 -0500

    Primer linea de codigo
    Nombre del username

--- cypress/e2e/prueba1.cy.js
+++ cypress/e2e/prueba1.cy.js
@@ -3,6 +3,8 @@ describe('registro en buggy', () => {
   it('haremos el registro de un usuario', () => {
     cy.visit('https://buggy.justtestit.org')
     cy.get('.btn-success-outline').click()
+    cy.get('#username').type('username2026') // JuanP
+
 
   })
 })
