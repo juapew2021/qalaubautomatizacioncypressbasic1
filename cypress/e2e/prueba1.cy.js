7c04e4f24706e2ca03ee232f1de7b1f22a98a2ee
Mateogomez8 <mateocast8@icloud.com>
2024-11-14 23:53:35 +0100

    ..

--- cypress/e2e/prueba1.cy.js
+++ cypress/e2e/prueba1.cy.js
@@ -1,24 +1,24 @@
 /// <reference types="Cypress"/>
 describe('registro en buggy', () => {
-  it.only('Haremos el registro de un usuario', () => {
+  it.skip('Haremos el registro de un usuario', () => {
     cy.visit('https://buggy.justtestit.org')
     cy.get('.btn-success-outline').click()
     cy.get('.btn-success-outline').click();
     cy.get('.col-md-6 > form > :nth-child(1)').click()
-    cy.get('#username').type ('Nathaniso7');
+    cy.get('#username').type ('Lucas2346');
     cy.get('.col-md-6 > form > :nth-child(2)').click()
-    cy.get('#firstName').type ('nathalitus')
+    cy.get('#firstName').type ('Lucas')
     cy.get('.col-md-6 > form > :nth-child(3)').click()
-    cy.get('#lastName').type ('Ninoso');
+    cy.get('#lastName').type ('Caste');
     cy.get('form > :nth-child(4)').click()
-    cy.get('#password').type ('Carlos123@')
+    cy.get('#password').type ('Carlos1234@')
     cy.get('form > :nth-child(5)').click()
-    cy.get('#confirmPassword').type ('Carlos123@')
+    cy.get('#confirmPassword').type ('Carlos1234@')
     cy.get('.btn-default').click()
-    cy.get('.result').should('include.text', 'Registration is successful')
+    cy.get('.result').should('contain.text', 'Registration is successful')
 
   })
-  it('Que no se registre con el mismo usuario', () => {
+  it.skip('Que no se registre con el mismo usuario', () => {
     cy.visit('https://buggy.justtestit.org')
     cy.get('.btn-success-outline').click()
     cy.get('.btn-success-outline').click();
@@ -33,6 +33,12 @@ describe('registro en buggy', () => {
     cy.get('form > :nth-child(5)').click()
     cy.get('#confirmPassword').type ('Carlos123@')
     cy.get('.btn-default').click()
+})
+
+it('Entrar a la pagina web con el nuevo usuario', () => {
+  cy.get('input').type('login')
+  cy.get('.form-inline > .form-group > [name="password"]').type('Carlos123@')
+  cy.get('.btn-success').click()
 })
 })
 
